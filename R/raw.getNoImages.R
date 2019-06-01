096901e70efd46abc216661cbab1114c764e6a8c
Thomas Gredig <thomas.gredig@csulb.edu>
2019-06-01 15:41:52 -0700

    add function to get number of images

---
+++ R/raw.getNoImages.R
@@ -0,0 +1,14 @@
+#' returns the number of images
+#'
+#' @param pfad path to the RAW folder
+#' @return number of images
+#' @examples
+#' raw.getNoImages('.')
+#'
+#' @export
+raw.getNoImages <- function(pfad) {
+  file.list = tolower(dir(pfad))
+  length(grep('jpg$', file.list)) +
+    length(grep('png$', file.list)) +
+    length(grep('jpeg$', file.list))
+}
