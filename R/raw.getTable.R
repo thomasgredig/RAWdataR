c271299683f5fd98b8569a21cba06a916abd253d
Thomas Gredig <thomas.gredig@csulb.edu>
2019-12-16 21:21:48 -0800

    limit RAW table to project name

--- R/raw.getTable.R
+++ R/raw.getTable.R
@@ -1,13 +1,18 @@
 #' returns a dataframe with field information for each file
 #'
 #' @param pfad path to the RAW folder
+#' @param project project name limits files
 #' @return dataframe
 #' @examples
 #' raw.getTable('.')
 #'
 #' @export
-raw.getTable <- function(pfad) {
-  file.list = dir(pfad,pattern='^[^_]')
+raw.getTable <- function(pfad, project='') {
+  if (project=='') {
+    file.list = dir(pfad,pattern='^[^_]')
+  } else {
+    file.list = dir(pfad, paste0('.*_',project,'_'))
+  }
   d = data.frame()
   for(f in file.list) {
     d = rbind(d, raw.splitFilename(f))
