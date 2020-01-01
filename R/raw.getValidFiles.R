9fe4049ed6837c93259fad9df6ae953f19cf7bf8
Thomas Gredig <thomas.gredig@csulb.edu>
2019-12-31 22:19:55 -0800

    return invalid files

--- R/raw.getValidFiles.R
+++ R/raw.getValidFiles.R
@@ -10,3 +10,17 @@ raw.getValidFiles <- function(pfad) {
   file.list = dir(pfad,pattern='^[^_]')
   file.list[sapply(strsplit(file.list,'_'),length)==6]
 }
+
+
+#' returns all invalid RAW files in a folder
+#'
+#' @param pfad path to the RAW folder
+#' @return vector with filenames
+#' @examples
+#' raw.getInvalidFiles('.')
+#'
+#' @export
+raw.getInvalidFiles <- function(pfad) {
+  file.list = dir(pfad,pattern='^[^_]')
+  file.list[sapply(strsplit(file.list,'_'),length)!=6]
+}
