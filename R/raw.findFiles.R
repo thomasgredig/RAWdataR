b83c98aff3eb2d9ed6406bf124b362ab85e13f59
Thomas Gredig <thomas.gredig@csulb.edu>
2020-03-29 17:52:20 -0700

    accept files with descriptions that have underscores

--- R/raw.findFiles.R
+++ R/raw.findFiles.R
@@ -8,6 +8,7 @@
 #' @param sample sample name
 #' @param md5 single string with comma separated abbreviated MD5 sums
 #' @param fullPath if TRUE, returns full path, otherwise filename only
+#' @param underscoreComments if TRUE, comments can have underscores
 #' @return list with filenames with certain checksum
 #' @examples
 #' file.list = raw.findFiles(pfad, date='201901')  # all files from Jan 2019
@@ -18,10 +19,11 @@
 raw.findFiles <- function(pfad, project='.*', date='.*',
                           user='.*', instrument='.*',
                           sample='.*',md5='',
-                          fullPath = TRUE) {
+                          fullPath = TRUE,
+                          underscoreComments = FALSE) {
   if ((date!='.*') & (nchar(date)<8)) { date=paste0(date,'.*') }
   muster = paste0(date, '_',project,'_',user,'_',instrument,'_',sample)
-  f = raw.validateFiles(dir(pfad, pattern=muster, ignore.case = TRUE))
+  f = raw.validateFiles(dir(pfad, pattern=muster, ignore.case = TRUE), underscoreComments)
   # check MD5 of those files and return only those files that agree
   if(nchar(md5)>0) {
     md5v = strsplit(md5,',')[[1]] # verify that those are the files
@@ -33,7 +35,11 @@ raw.findFiles <- function(pfad, project='.*', date='.*',
   f
 }
 
-raw.validateFiles <- function(file.list) {
+raw.validateFiles <- function(file.list, underscoreComments) {
   f1 = file.list[sapply(strsplit(file.list,'_'),length)>=5]
-  f1[sapply(strsplit(f1,'_'),length)<=6]
+  if (underscoreComments==TRUE) {
+    f1
+  } else {
+    f1[sapply(strsplit(f1,'_'),length)<=6]
+  }
 }
