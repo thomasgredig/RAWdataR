168e85979757806d6ab8643bd28c5b824af889bc
Thomas Gredig <thomas.gredig@csulb.edu>
2021-09-24 16:00:13 -0700

    get number of samples to include comments with underscore by default

--- R/raw.getNoSamples.R
+++ R/raw.getNoSamples.R
@@ -1,12 +1,13 @@
 #' returns approximate number of samples that were made
 #'
 #' @param pfad path to the RAW folder
+#' @param underscoreComments include files with comments that have _
 #' @return number of samples that were made with the NTE
 #' @examples
 #' raw.getNoSamples('.')
 #'
 #' @export
-raw.getNoSamples <- function(pfad) {
-  file.list = raw.findFiles(pfad, instrument='NTE')
+raw.getNoSamples <- function(pfad, underscoreComments=TRUE) {
+  file.list = raw.findFiles(pfad, instrument='NTE', underscoreComments)
   length(file.list)
 }
