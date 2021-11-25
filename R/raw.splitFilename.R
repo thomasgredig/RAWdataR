e6498ce98609355f4145464ce229687dd918b889
Thomas Gredig <thomas.gredig@csulb.edu>
2021-11-24 18:27:00 -0800

    fix splitFilename so that it splits it correctly for different samples

--- R/raw.splitFilename.R
+++ R/raw.splitFilename.R
@@ -29,44 +29,51 @@ empty.table = data.frame(
 #'
 #' @export
 raw.splitFilename <- function(filename) {
-  s = strsplit.max(filename,'_',6)
-  if (length(s)<6) {
-    e = empty.table
-    e$Filename = filename
-    if (length(s)==5) {
-      Extension = gsub('.*\\.([^.]+)$','\\1',s[5])
-      sample =  gsub('(.*)\\.[^.]+$','\\1',s[5])
-      q = strsplit.max(sample,'-',2)
-      if(length(q)==2) {
-        s[5] = q[1]
-        desc = paste0(q[2],'.',Extension)
-      } else {
-        s[5] = sample
-        desc = paste0('.',Extension)
-      }
-      e = data.frame(
-        Date = s[1],
-        Date.formatted = as.Date(s[1], format='%Y%m%d'),
-        Project = s[2],
-        User = s[3],
-        Instrument = s[4],
-        Sample = s[5],
-        Description = gsub('(.*)\\.[^.]+$','\\1',desc),
-        Extension = gsub('.*\\.([^.]+)$','\\1',desc),
-        Filename = filename
-      )
-    }
-    return(e)
-  }
-  data.frame(
-    Date = s[1],
-    Date.formatted = as.Date(s[1], format='%Y%m%d'),
-    Project = s[2],
-    User = s[3],
-    Instrument = s[4],
-    Sample = s[5],
-    Description = gsub('(.*)\\.[^.]+$','\\1',s[6]),
-    Extension = gsub('.*\\.([^.]+)$','\\1',s[6]),
-    Filename = filename
-  )
+  d1 = as.data.frame(t(sapply(filelist, FUN=function(x) { strsplit.max(basename(x),'_',6) } )))
+  rownames(d1) <- NULL
+  names(d1) = c('Date','Project','User','Instrument','Sample','Description')
+  d1$Extension = tools::file_ext(filelist)
+  d1
 }
+  # d1 = as.data.frame(t(sapply(filelist, FUN=function(x) { strsplit.max(x,'_',6) } )))
+  # s = strsplit.max(filename,'_',6)
+  # if (length(s)<6) {
+  #   e = empty.table
+  #   e$Filename = filename
+  #   if (length(s)==5) {
+  #     Extension = gsub('.*\\.([^.]+)$','\\1',s[5])
+  #     sample =  gsub('(.*)\\.[^.]+$','\\1',s[5])
+  #     q = strsplit.max(sample,'-',2)
+  #     if(length(q)==2) {
+  #       s[5] = q[1]
+  #       desc = paste0(q[2],'.',Extension)
+  #     } else {
+  #       s[5] = sample
+  #       desc = paste0('.',Extension)
+  #     }
+  #     e = data.frame(
+  #       Date = s[1],
+  #       Date.formatted = as.Date(s[1], format='%Y%m%d'),
+  #       Project = s[2],
+  #       User = s[3],
+  #       Instrument = s[4],
+  #       Sample = s[5],
+  #       Description = gsub('(.*)\\.[^.]+$','\\1',desc),
+  #       Extension = gsub('.*\\.([^.]+)$','\\1',desc),
+  #       Filename = filename
+  #     )
+  #   }
+  #   return(e)
+  # }
+  # data.frame(
+  #   Date = s[1],
+  #   Date.formatted = as.Date(s[1], format='%Y%m%d'),
+  #   Project = s[2],
+  #   User = s[3],
+  #   Instrument = s[4],
+  #   Sample = s[5],
+  #   Description = gsub('(.*)\\.[^.]+$','\\1',s[6]),
+  #   Extension = gsub('.*\\.([^.]+)$','\\1',s[6]),
+  #   Filename = filename
+  # )
+# }
