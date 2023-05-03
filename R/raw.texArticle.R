d5203b4d6ff85456697776a865094beecc0c1971
Thomas Gredig <thomas.gredig@csulb.edu>
2023-05-03 09:36:27 -0700

    clean up raw.texArticle and fig2tex descriptions

--- R/raw.texArticle.R
+++ R/raw.texArticle.R
@@ -1,9 +1,17 @@
 #' LaTeX Article with all Fig2TeX figures
 #'
+#' @description
+#' After saving TeX snippets into a folder (\code{figPath}) with \code{\link{fig2tex}},
+#' then, use this function to generate
+#' a main TeX file called \code{fileTeX} that includes all the snippets and after
+#' compiling shows a document with all the graphs / images. The main TeX file is
+#' saved in the same folder as the \code{figPath}. It will overwrite a file.
+#'
+#'
 #' @param figPath path for figures, should contain tex figure files generated with \code{fig2tex}
 #' @param fileTex name of article that is generated in figPath
 #'
-#' @seealso \code{\link{fig2tex()}}
+#' @seealso \code{\link{fig2tex}}
 #'
 #' @export
 raw.texArticle <- function(figPath,
